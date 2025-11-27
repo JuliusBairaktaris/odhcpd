4ff35533494ea5b531540a69e90ede1124eaede7
David Härdeman <david@hardeman.nu>
2025-11-27 11:35:09 +0100

    dhcpv6-ia: fix realloc bug
    Commit b9db4d7061a08bf82a25222074065cce71973d0c introduced a bug, the
    "hostname" variable used for the realloc would shadow the real hostname
    defined at the beginning of the function. Fix this by using a different
    variable name.
    
    Fixes: b9db4d7061a0 ("dhcpv6: handle realloc failure")
    Signed-off-by: David Härdeman <david@hardeman.nu>
    Link: https://github.com/openwrt/odhcpd/pull/326
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    (cherry picked from commit d21e504b38ab4c880c43b7f1649104bb2f0d2d8b)

--- src/dhcpv6-ia.c
+++ src/dhcpv6-ia.c
@@ -1636,9 +1636,9 @@ ssize_t dhcpv6_ia_handle_IAs(uint8_t *buf, size_t buflen, struct interface *ifac
 				    hdr->msg_type == DHCPV6_MSG_REQUEST ||
 				    hdr->msg_type == DHCPV6_MSG_REBIND)) {
 				if ((!(a->flags & OAF_STATIC) || !a->hostname) && hostname_len > 0) {
-					char *hostname = realloc(a->hostname, hostname_len + 1);
-					if (hostname) {
-						a->hostname = hostname;
+					char *tmp = realloc(a->hostname, hostname_len + 1);
+					if (tmp) {
+						a->hostname = tmp;
 						memcpy(a->hostname, hostname, hostname_len);
 						a->hostname[hostname_len] = 0;
 
